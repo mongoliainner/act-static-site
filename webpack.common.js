2fd27c76d3f364b1991a2852d9ecf8f325c9bbda
= <yuexrender@gmail.com>
2024-10-28 14:39:01 +0300

    added: x3

--- webpack.common.js
+++ webpack.common.js
@@ -23,7 +23,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-      filename: "index.html",
+      filename: "index.html", // this should create an index.html in the dist folder
     }),
   ],
 };
