479c4daf90be4fe8e7f51bf70f30fe6c249b1f2a
= <yuexrender@gmail.com>
2024-10-28 14:28:42 +0300

    Added: something

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,22 @@
-const { merge } = require('webpack-merge')
-const common = require('./webpack.common.js')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = merge(common, {
-  mode: 'production'
-})
+module.exports = {
+  entry: "./src/index.js", // Make sure this points to your main JavaScript file
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "./",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // Path to your main HTML file
+      filename: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      // Define loaders here (e.g., for CSS, images)
+    ],
+  },
+};
